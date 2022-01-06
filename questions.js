14292e74050825506c5a9b9f062e31ccddaca13c
ritikkasotiya <hrithikkasotiya8529@gmail.com>
2022-01-07 00:11:46 +0530

    first commit
    This is quiz App with timer using html,css, javascript.

---
+++ questions.js
@@ -0,0 +1,64 @@
+// create an array of questions
+
+let questions = [
+    {
+        numb : 1,
+        question: "what does html stands for ?",
+        answer: "hyper text markup language",
+        options: [
+            "host multiple language",
+            "hypet text preprocessor",
+            "hyper text markup language",
+            "hyper tool multi language"
+        ]
+
+    },
+    {
+        numb : 2,
+        question: "what does css stands for ?",
+        answer: "cascading style sheet",
+        options: [
+            "host multiple language",
+            "cascading style sheet",
+            "hyper text markup language",
+            "hyper tool multi language"
+        ]
+
+    },
+    {
+        numb : 3,
+        question: "what does php stands for ?",
+        answer: "hypertext preprocessor",
+        options: [
+            "host multiple language",
+            "hypet tool preprocessor",
+            "hypertext preprocessor",
+            "hyper tool multi language"
+        ]
+
+    },
+    {
+        numb : 4,
+        question: "what does sql stands for ?",
+        answer: "structured querry language",
+        options: [
+            "host multiple language",
+            "hypet text preprocessor",
+            "hyper text markup language",
+            "structured querry language"
+        ]
+
+    },
+    {
+        numb : 5,
+        question: "what is the surname of ritik?",
+        answer: "kasotiya",
+        options: [
+            "bhadana",
+            "chauhan",
+            "jaatav",
+            "kasotiya"
+        ]
+
+    }
+];
